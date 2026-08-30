48270754f0e54482d9940d6f8e5566c8fe942b89
LeyvinoBezerra <Leyvinobezerra.develop@gmail.com>
2026-08-30 20:04:01 -0300

    fisrt commit

---
+++ meavalie/src/main/java/com/meavalie/MeavalieApplication.java
@@ -0,0 +1,13 @@
+package com.meavalie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeavalieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeavalieApplication.class, args);
+	}
+
+}
---
+++ meavalie/src/test/java/com/meavalie/MeavalieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meavalie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeavalieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
